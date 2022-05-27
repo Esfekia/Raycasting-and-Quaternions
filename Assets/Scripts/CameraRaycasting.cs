06e537094002e1e903f0e9b64720dd61ea06fa4b
Esfekia <sertac@gmail.com>
2022-05-27 05:56:27 -0400

    Added CameraRraycasting script which now changes the color of the recipient to yellow when raycast on.
    Added CameraRraycasting script which now changes the color of the recipient to yellow when raycast on.

---
+++ Assets/Scripts/CameraRaycasting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycasting : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        // hit variable to store our results
+        RaycastHit hit;
+
+        // a ray that goes from screen coordinates forward into our world
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // coundct our Raycast
+        if (Physics.Raycast(ray, out hit))
+        {
+            hit.collider.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+        }
+        else
+        {
+            Debug.Log("We didn't hit anything.");
+        }
+    }
+}
