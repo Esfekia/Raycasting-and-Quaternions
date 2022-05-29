58b334ea8775c7c8642bfc1e1a8fd26218b25425
Esfekia <sertac@gmail.com>
2022-05-29 05:54:01 -0400

    Updated CameraRaycasting.cs to change the debug.log to happen only when our ray is cast and does not hit anything, rather than at every frame.
    Updated CameraRaycasting.cs to change the debug.log to happen only when our ray is cast and does not hit anything, rather than at every frame.

--- Assets/Scripts/CameraRaycasting.cs
+++ Assets/Scripts/CameraRaycasting.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRaycasting : MonoBehaviour
-{    
+{
+    // hit variable to store our results
+    RaycastHit hit;
+
+    Ray ray;
+
     private void FixedUpdate()
     {
-        // hit variable to store our results
-        RaycastHit hit;
-
-        // a ray that goes from screen coordinates forward into our world
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        // coundct our Raycast
-        if (Physics.Raycast(ray, out hit))
-        {
-            
-            hit.collider.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);       
-                        
-        }
-        else
-        {
-            Debug.Log("We didn't hit anything.");
-        }
         
+        // check for mouse input first
+        if (Input.GetMouseButtonDown(0))
+        {
+            // a ray that goes from screen coordinates forward into our world
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // coundct our Raycast
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+
+            }
+            else
+            {
+                Debug.Log("We didn't hit anything.");
+            }
+
+        }
+                     
         
     }
 }
